0bc5b8d8bcfd5bd08023cb04c7220353f74ff40d
Rido <rido@airmail.cc>
2021-03-02 13:55:13 +1100

    added new status scripts

--- config.h
+++ config.h
@@ -1,13 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:",      "memory",    30,    1},
-	{"",          "datetime",   5,    2},
-	{" Network", "internet",   5,    3},
+	{"",      "weather",    1800,    5},
+	//{"",      "forecast",    300,    5},
+	{"^c#F7768E^Mem:^d^ ",      "memory",    30,    1},
+	{"^c#9EcE6A^",          "datetime",   5,    2},
+	{"^c#4abaaf^", "internet",   0,    3},
+	{"^c#4abaaf^", "volume",   0,    4},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " | ";
+static char *delim = "^c#a9b1d6^ | ^d^";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
